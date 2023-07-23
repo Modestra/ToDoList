6e898046318d5f94a687e006aea7c8e9cffa87b6
Kirill <104451159+Modestra@users.noreply.github.com>
2023-07-23 18:49:22 +0700

    Создание компонентов и верстки

---
+++ src/app/createtask/createtask.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'createtask',
+  templateUrl: './createtask.component.html',
+  styleUrls: ['./createtask.component.css']
+})
+export class CreatetaskComponent {
+
+}
