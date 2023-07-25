a5bcda78f66976e7fef3d15dc77f967ef1b45cd0
Kirill <104451159+Modestra@users.noreply.github.com>
2023-07-25 16:21:10 +0700

    Backend компонентов

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,19 @@
-import {Component, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'task-ul',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   list:string[] = [];
   constructor() {
 
   }
-  ngOnInit(){
-
-  }
   createTask({option}: { option: string }){
     this.tasks.push(option)
-    console.log(this.list)
   }
+
   @Output() tasks = this.list;
+  @Output() onClose: EventEmitter<boolean> = new EventEmitter();
 }
--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, DoCheck, Output, EventEmitter} from '@angular/core';
+import {AppComponent} from "../app.component";
 
 @Component({
   selector: 'task',
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.css']
 })
-export class TaskComponent {
+export class TaskComponent implements OnInit, DoCheck{
   taskname: any;
+  status:string;
+  showModal:boolean = true;
+  color:string = "while";
+  isfavorite:boolean = false;
+  star:string = "assets/star.png"
+  ToProcess(){
+    this.status = "process";
+  }
+  ngOnInit() {
 
+  }
+  ngDoCheck() {
+    if (this.status == "process"){
+      this.color = "rgba(255,255,36,0.73)";
+    }
+    else if(this.status == "ended"){
+      this.color = "rgba(0,255,0,0.54)";
+    }
+    else {
+
+    }
+  }
+
+  ToComplete(){
+    this.status = "ended";
+  }
+  IsFavorite(){
+    if(this.isfavorite == false){
+      this.star = "assets/favourite.png";
+      this.isfavorite = true;
+    }
+    else {
+      this.isfavorite = false;
+      this.star = "assets/star.png";
+    }
+  }
 }
